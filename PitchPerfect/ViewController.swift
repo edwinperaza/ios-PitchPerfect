078ba2ccaa983eaa095224abbbacf4796e5fd769
edwinperaza <edwinperaza87@gmail.com>
2017-03-02 17:26:54 -0300

    add new viewController and also save audio and pass URL via segue

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -57,10 +57,21 @@ class ViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        print("Finished recording")
+        if flag {
+           performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("recording was not successful")
+        }
+        
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundVC.recordedAudioURL = recordedAudioURL
+        }
+    }
     
 }
 
